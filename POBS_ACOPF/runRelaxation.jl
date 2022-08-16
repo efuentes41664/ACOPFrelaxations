75fc045a24ce551e41b50705bdc80e13e1e30fb3
efuentes41664 <111385158+efuentes41664@users.noreply.github.com>
2022-08-16 13:44:23 -0500

    Update runRelaxation.jl

--- POBS_ACOPF/runRelaxation.jl
+++ POBS_ACOPF/runRelaxation.jl
@@ -11,18 +11,3 @@ include("classicSDP.jl")
 include("tightening.jl")
 optimize!(model)
 
-using LinearAlgebra
-eigval2 = []
-for k in keys(K)
-    Mval = value.([
-        y[ (if i == 0 sparse([1],[1],[0],2n_bus-1,1) 
-            else sparse([i],[1],[1],2n_bus-1,1) end +
-            if j == 0 sparse([1],[1],[0],2n_bus-1,1) 
-            else sparse([j],[1],[1],2n_bus-1,1) end 
-        )]
-        for i in [K[k]; 0], j in [K[k]; 0]
-    ])
-    global eigval2 = [eigval2; eigvals(Mval)[end-1] ]
-    #println(eigvals(Mval))
-end 
-println(case," ",baseMVA*objective_value(model)," ",solve_time(model)," ",maximum(eigval2))
